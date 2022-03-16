fd8eece81f1b875cd88ccceba1c0229058880d0e
liununu <liununu5@gmail.com>
2022-03-16 19:54:16 +0800

    build(spring-contract-provider): init project

---
+++ spring-contract-provider/src/test/kotlin/me/liununu/springcontractprovider/SpringContractProviderApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.liununu.springcontractprovider
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringContractProviderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
