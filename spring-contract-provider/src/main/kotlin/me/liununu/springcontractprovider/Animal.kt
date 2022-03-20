740d1a629057071d9c2d3d7742bd0a67e3631c23
liununu <liununu5@gmail.com>
2022-03-20 19:16:47 +0800

    feat(spring-contract-provider): WIP: add the model of Animal

---
+++ spring-contract-provider/src/main/kotlin/me/liununu/springcontractprovider/Animal.kt
@@ -0,0 +1,30 @@
+package me.liununu.springcontractprovider
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(SnakeCaseStrategy::class)
+data class Animal(
+    val firstName: String,
+    val lastName: String,
+    val animal: String,
+    val age: Int,
+    val availableFrom: String,
+    val gender: String,
+    val location: Location,
+    val eligibility: Eligibility,
+    val interests: List<String>,
+)
+
+@JsonNaming(SnakeCaseStrategy::class)
+data class Eligibility(
+    val available: Boolean,
+    val previouslyMarried: Boolean,
+)
+
+@JsonNaming(SnakeCaseStrategy::class)
+data class Location(
+    val description: String,
+    val country: String,
+    val postCode: Int,
+)
