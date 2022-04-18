b2e6c40376b13941af661ae2db445e47bac5bab0
liununu <liununu5@gmail.com>
2022-04-18 22:29:44 +0800

    feat(spring-mongodb): init project

---
+++ spring-mongodb/src/main/kotlin/me/liununu/springmongodb/SpringMongodbApplication.kt
@@ -0,0 +1,11 @@
+package me.liununu.springmongodb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMongodbApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringMongodbApplication>(*args)
+}
---
+++ spring-mongodb/src/test/kotlin/me/liununu/springmongodb/SpringMongodbApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.liununu.springmongodb
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMongodbApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
