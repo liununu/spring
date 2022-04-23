4e70edfb4a54b0233f39538bf4f8665f61dfcdec
liununu <liununu5@gmail.com>
2022-04-24 01:42:18 +0800

    test(spring-mongodb): setup mongodb testcontainers

--- spring-mongodb/src/test/kotlin/me/liununu/springmongodb/SpringMongodbApplicationTests.kt
+++ spring-mongodb/src/test/kotlin/me/liununu/springmongodb/SpringMongodbApplicationTests.kt
@@ -1,13 +1,21 @@
 package me.liununu.springmongodb
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.testcontainers.containers.MongoDBContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
+@Testcontainers
 class SpringMongodbApplicationTests {
 
-    @Test
-    fun contextLoads() {
-    }
+    @Container
+    private val container: MongoDBContainer = MongoDBContainer("mongo:latest")
 
+    @Test
+    internal fun shouldInitMongoDBContainer() {
+        assertThat(container.isRunning).isTrue
+    }
 }
