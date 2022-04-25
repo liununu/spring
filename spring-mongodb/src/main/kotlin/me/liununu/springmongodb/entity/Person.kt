c39fab9f7a9aacf1a2fb6e17d774ad5b2237ff0f
liununu <liununu5@gmail.com>
2022-04-25 21:00:09 +0800

    feat(spring-mongodb): init mongodb data

--- spring-mongodb/src/main/kotlin/me/liununu/springmongodb/entity/Person.kt
+++ spring-mongodb/src/main/kotlin/me/liununu/springmongodb/entity/Person.kt
@@ -3,7 +3,7 @@ package me.liununu.springmongodb.entity
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
-@Document
+@Document("persons")
 data class Person(
     @Id val id: String,
     val firstName: String,
---
+++ spring-mongodb/src/test/resources/init/persons-init.js
@@ -0,0 +1,43 @@
+db = db.getSiblingDB('test');
+db.createCollection('persons');
+db.persons.insertMany([{
+    "first_name": "Cacilia",
+    "last_name": "Bricknell",
+    "email": "cbricknell0@ezinearticles.com"
+}, {
+    "first_name": "Chariot",
+    "last_name": "Pendlebury",
+    "email": "cpendlebury1@census.gov"
+}, {
+    "first_name": "Florinda",
+    "last_name": "Baxster",
+    "email": "fbaxster2@nationalgeographic.com"
+}, {
+    "first_name": "Dennison",
+    "last_name": "Lamacraft",
+    "email": "dlamacraft3@google.nl"
+}, {
+    "first_name": "Aimil",
+    "last_name": "Leishman",
+    "email": "aleishman4@tumblr.com"
+}, {
+    "first_name": "Sheba",
+    "last_name": "Deane",
+    "email": "sdeane5@pen.io"
+}, {
+    "first_name": "Timothea",
+    "last_name": "Agglione",
+    "email": "tagglione6@slashdot.org"
+}, {
+    "first_name": "Kellyann",
+    "last_name": "MacNamee",
+    "email": "kmacnamee7@goo.gl"
+}, {
+    "first_name": "Antoni",
+    "last_name": "Scargle",
+    "email": "ascargle8@dmoz.org"
+}, {
+    "first_name": "Tammara",
+    "last_name": "Rickersy",
+    "email": "trickersy9@springer.com"
+}]);
