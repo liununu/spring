edb9870610faa0ad273235eb16b4eba0b3e47afd
liununu <liununu5@gmail.com>
2023-05-14 02:13:22 +0800

    feat(spring-batch): update existing client by id

--- spring-batch/src/main/kotlin/me/liununu/springbatch/output/Client.kt
+++ spring-batch/src/main/kotlin/me/liununu/springbatch/output/Client.kt
@@ -1,6 +1,8 @@
 package me.liununu.springbatch.output
 
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.Id
 import java.time.ZonedDateTime
 
@@ -9,7 +11,7 @@ data class Client(
     @Id val id: Int,
     val name: String,
     val email: String,
-    val gender: Gender,
+    @Enumerated(EnumType.STRING) val gender: Gender,
     val remark: String,
     val ipAddress: String,
     val createdAt: ZonedDateTime,
---
+++ spring-batch/src/test/resources/client.sql
@@ -0,0 +1,2 @@
+insert into client (created_at, email, gender, ip_address, name, remark, id)
+values ('2022-06-21T00:18:08+08:00[Asia/Hong_Kong]', 'any', 'FEMALE', '127.0.0.1', 'any', 'any', 1);
