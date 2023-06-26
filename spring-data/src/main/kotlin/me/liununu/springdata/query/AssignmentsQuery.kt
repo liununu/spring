3849a21a1a9c53faccbbf7349a49f79490e35d11
liununu <liununu5@gmail.com>
2023-06-27 02:23:40 +0800

    feat(spring-data): get finder from AssignmentsFinderFactory in AssignmentsQuery

--- spring-data/src/main/kotlin/me/liununu/springdata/query/AssignmentsQuery.kt
+++ spring-data/src/main/kotlin/me/liununu/springdata/query/AssignmentsQuery.kt
@@ -1,10 +1,9 @@
 package me.liununu.springdata.query
 
 import me.liununu.springdata.query.finder.AssignmentsFinderFactory
-import me.liununu.springdata.query.finder.NoopFinder
 import org.springframework.stereotype.Service
 
 @Service
-class AssignmentsQuery(private val finderFactory: AssignmentsFinderFactory, private val finder: NoopFinder) {
-    fun findAll() = this.finder.findAll()
+class AssignmentsQuery(private val finderFactory: AssignmentsFinderFactory) {
+    fun findAll() = this.finderFactory.get().findAll()
 }
--- spring-data/src/test/kotlin/me/liununu/springdata/query/AssignmentsQueryTest.kt
+++ spring-data/src/test/kotlin/me/liununu/springdata/query/AssignmentsQueryTest.kt
@@ -1,16 +1,27 @@
 package me.liununu.springdata.query
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junitpioneer.jupiter.SetEnvironmentVariable
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class AssignmentsQueryTest {
+class AssignmentsQueryTest(@Autowired private val query: AssignmentsQuery) {
     @Test
-    fun `should return empty when find all given noop finder`(@Autowired query: AssignmentsQuery) {
+    @SetEnvironmentVariable(value = "NOOP", key = "ACTIVE_FINDER")
+    fun `should return empty when find all given noop finder`() {
         val assignments = query.findAll()
 
         assertThat(assignments).isEmpty()
     }
+
+    @Test
+    fun `should throw when find all given not implemented finder`() {
+        assertThatThrownBy { query.findAll() }
+            .isExactlyInstanceOf(NotImplementedError::class.java)
+            .hasMessage("Not yet implemented")
+    }
+
 }
